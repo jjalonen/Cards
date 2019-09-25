d004e4f9108c92e192dc601dc1a2a4e5c865157b
Joonas Jalonen <joonas.jalzi@gmail.com>
2019-09-25 10:21:42 +0300

    added add address and save feature

--- connect.php
+++ connect.php
@@ -1,12 +1,10 @@
 <?php
 function connect_db() {
  $servername = "localhost";
- $username = "cards";
+ $username = "joonas";
  $password = "password";
- $dbname = "cards";
+ $dbname = "joonas";
  // Create connection
-GIT Exercises 4 (13)
-29.12.2018
  $conn = new mysqli($servername, $username, $password, $dbname);
  // Check connection
  if ($conn->connect_error) {
